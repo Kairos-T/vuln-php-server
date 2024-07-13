fdab5d8d6df67aa7dc1a621e41d4cdc38af90505
Kairos-T <contact@kairostay.com>
2024-07-13 16:08:15 +0800

    chore: remove unused functions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,43 +1,11 @@
 #!/bin/bash
 
-# Move index.html and server.php to /var/www/html
-sudo mv index.html /var/www/html
-sudo mv server.php /var/www/html
-sudo mkdir /var/www/html/uploads
-sudo chmod 777 /var/www/html/uploads
-
 # Install Apache and Nginx
 sudo apt update
 sudo apt install -y apache2
 sudo apt install -y nginx
 
-# Change Apache listening port from 80 to 8080
-APACHE_PORTS_CONF="/etc/apache2/ports.conf"
-APACHE_SITES_AVAILABLE="/etc/apache2/sites-available/000-default.conf"
-
-change_apache_port() {
-    local conf_file=$1
-    local old_port=$2
-    local new_port=$3
-
-    sudo sed -i "s/Listen $old_port/Listen $new_port/" "$conf_file"
-}
-
-update_virtual_host() {
-    local conf_file=$1
-    local old_port=$2
-    local new_port=$3
-
-    sudo sed -i "s/<VirtualHost *:$old_port>/<VirtualHost *:$new_port>/" "$conf_file"
-}
-
-change_apache_port "$APACHE_PORTS_CONF" 80 8080
-update_virtual_host "$APACHE_SITES_AVAILABLE" 80 8080
-
-sudo systemctl restart apache2
-
 # Create Nginx config file
-#!/bin/bash
 
 DOMAIN="server.cure51.com"
 SERVER_IP="192.168.1.2"
